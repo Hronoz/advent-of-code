923caf20a4d60bcaadd2bc344bdf578a7c44bd98
Hronoz <hronozovichhronoz@gmail.com>
2023-06-12 11:38:13 +0300

    Day 2 Part 2 completed

--- c/2015/day_2/main.c
+++ c/2015/day_2/main.c
@@ -5,6 +5,28 @@
 #define AOCYEAR 2015
 #define AOCDAY 2
 
+int find_surface_area(int width, int height, int length);
+int find_smallest_side(int width, int height, int length);
+int find_smallest_perimeter(int width, int height, int length);
+
+int main()
+{
+  int width, height, length;
+  int paper_quantity = 0;
+  int ribbon_quantity = 0;
+  while (scanf("%dx%dx%d", &width, &height, &length) == 3)
+  {
+    paper_quantity += find_surface_area(width, height, length);
+    paper_quantity += find_smallest_side(width, height, length);
+    ribbon_quantity += width * height * length;
+    ribbon_quantity += find_smallest_perimeter(width, height, length);
+  }
+  printf("%4d-%02d/%d: %d\n", AOCYEAR, AOCDAY, 1, paper_quantity);
+  printf("%4d-%02d/%d: %d\n", AOCYEAR, AOCDAY, 2, ribbon_quantity);
+
+  return 0;
+}
+
 int find_surface_area(int width, int height, int length)
 {
   return 2 * width * height + 2 * width * length + 2 * height * length;
@@ -19,16 +41,11 @@ int find_smallest_side(int width, int height, int length)
   return x > y ? (y > z ? z : y) : (x > z ? z : x);
 }
 
-int main()
+int find_smallest_perimeter(int width, int height, int length)
 {
-  int width, height, length;
-  int paper_quantity = 0;
-  while (scanf("%dx%dx%d", &width, &height, &length) == 3)
-  {
-    paper_quantity += find_surface_area(width, height, length);
-    paper_quantity += find_smallest_side(width, height, length);
-  }
-  printf("%4d-%02d/%d: %d\n", AOCYEAR, AOCDAY, 1, paper_quantity);
+  int x = 2 * width + 2 * height;
+  int y = 2 * width + 2 * length;
+  int z = 2 * height + 2 * length;
 
-  return 0;
+  return x > y ? (y > z ? z : y) : (x > z ? z : x);
 }
